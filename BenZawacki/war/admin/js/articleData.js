a0a75dee1bbea85fd2e4e58e56a6537f27e03c61
Neil Poulin <neil.j.poulin@gmail.com>
2013-03-21 12:07:09 -0600

    updated to linkify

--- BenZawacki/war/admin/js/articleData.js
+++ BenZawacki/war/admin/js/articleData.js
@@ -72,7 +72,8 @@ function insertLink(){
 	var text = $("#link-text").val();
 	var html = linkify(address);	
 	html = $(html).html(text)[0].outerHTML;
-	console.log(html[0].outerHTML);
+	console.log(html);
+	html = html.replace(/"/gi, "");
 	$(".active .article.content").val($(".active .article.content").val() + html).trigger("keyup");
 	$("#link-modal").modal("hide");
 }
@@ -82,15 +83,15 @@ function linkify(inputText) {
 
     //URLs starting with http://, https://, or ftp://
     replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+    replacedText = inputText.replace(replacePattern1, '<a href=$1 target=_blank>$1</a>');
 
     //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
     replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+    replacedText = replacedText.replace(replacePattern2, '$1<a href=http://$2 target=_blank>$2</a>');
 
     //Change email addresses to mailto:: links.
     replacePattern3 = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/gim;
-    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+    replacedText = replacedText.replace(replacePattern3, '<a href=mailto:$1>$1</a>');
 
     return replacedText;
 }
